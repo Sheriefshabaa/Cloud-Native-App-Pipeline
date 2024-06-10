447eff2c0ca87b15d5082a91a64ff8e003094ab6
Hesham-14 <heshammuhammed14@gmail.com>
2024-06-11 00:52:39 +0300

    Editing files

--- app/images-build-push.sh
+++ app/images-build-push.sh
@@ -17,12 +17,12 @@ docker build -t backend-image ./backend
 
 # Tag the Docker images with the ECR repository URI
 echo "Tagging Docker images..."
-docker tag backend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${ECR_REPOSITORY_NAME}:latest
-docker tag frontend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${ECR_REPOSITORY_NAME}:latest
+docker tag backend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${BACKEND_ECR_REPOSITORY_NAME}:latest
+docker tag frontend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${FRONTEND_ECR_REPOSITORY_NAME}:latest
 
 # Push the Docker images to Amazon ECR
 echo "Pushing Docker images to Amazon ECR..."
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${ECR_REPOSITORY_NAME}:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${ECR_REPOSITORY_NAME}:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${BACKEND_ECR_REPOSITORY_NAME}:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${FRONTEND_ECR_REPOSITORY_NAME}:latest
 
 echo "Script execution completed."
