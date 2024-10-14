ec35e699eba55c678af257402efccac950eb6fab
sheriefshabaan <95883381+Sheriefshabaa@users.noreply.github.com>
2024-10-14 17:50:38 +0300

    Update images-build-push.sh
    - updating files to `images-build-push.sh` file to use a single repo
    - updated images tages to distinct the images

--- app/images-build-push.sh
+++ app/images-build-push.sh
@@ -2,9 +2,8 @@
 
 # Set AWS region and account ID
 AWS_REGION="eu-west-1"
-AWS_ACCOUNT_ID="767397735708"
-BACKEND_ECR_REPOSITORY_NAME="h-project-ecr-backend-image"
-FRONTEND_ECR_REPOSITORY_NAME="h-project-ecr-frontend-image"
+AWS_ACCOUNT_ID="171055612566"
+ECR_REPOSITORY_NAME="h-project-ecr-backend-image"
 
 # Authenticate Docker to the Amazon ECR registry
 echo "Authenticating Docker to Amazon ECR..."
@@ -17,12 +16,12 @@ docker build -t backend-image ./backend
 
 # Tag the Docker images with the ECR repository URI
 echo "Tagging Docker images..."
-docker tag backend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${BACKEND_ECR_REPOSITORY_NAME}:latest
-docker tag frontend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${FRONTEND_ECR_REPOSITORY_NAME}:latest
+docker tag backend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${ECR_REPOSITORY_NAME}:backend
+docker tag frontend-image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${ECR_REPOSITORY_NAME}:frontend
 
 # Push the Docker images to Amazon ECR
 echo "Pushing Docker images to Amazon ECR..."
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${BACKEND_ECR_REPOSITORY_NAME}:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${FRONTEND_ECR_REPOSITORY_NAME}:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${BACKEND_ECR_REPOSITORY_NAME}:backend
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${FRONTEND_ECR_REPOSITORY_NAME}:frontend
 
 echo "Script execution completed."
